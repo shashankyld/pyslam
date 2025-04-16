f232e13dbf07ee3e923c7189d370a02dc02991c3
shashankyld <shashankyld@gmail.com>
2025-04-16 04:58:51 +0200

    I tried both ways, checking idle is not the problem, I think how matches are calculated is a problem because of the rotation maybe

--- config_parameters.py
+++ config_parameters.py
@@ -24,7 +24,7 @@ class Parameters:
     
     # SLAM threads 
     kLocalMappingOnSeparateThread=True               # True: move local mapping on a separate thread, False: tracking and then local mapping in a single thread 
-    kTrackingWaitForLocalMappingToGetIdle=False      # True: wait for local mapping to be idle before starting tracking, False: tracking and then local mapping in a single thread  
+    kTrackingWaitForLocalMappingToGetIdle=True      # True: wait for local mapping to be idle before starting tracking, False: tracking and then local mapping in a single thread  
     kTrackingWaitForLocalMappingSleepTime=0.1        # DEPRECATED: -1 for no sleep # [s]    (NOTE: a bit of sleep time increases the call rate of LBA and therefore VO accuracy)
     kLocalMappingParallelKpsMatching=True
     kLocalMappingParallelKpsMatchingNumWorkers=6
