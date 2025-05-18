f812562be5d1fd90476df2df2131bbdcad6fbf76
shashankyld <shashankyld@gmail.com>
2025-05-18 12:56:46 +0200

    Ideally Delaunay should not change lets try to visualize on the ref frame and then delete the edges there and see.

--- local_features/feature_superpoint.py
+++ local_features/feature_superpoint.py
@@ -42,8 +42,8 @@ class SuperPointOptions:
         # default options from demo_superpoints
         self.weights_path=config.cfg.root_folder + '/thirdparty/superpoint/superpoint_v1.pth'
         print(f'SuperPoint weights: {self.weights_path}')
-        self.nms_dist=3
-        self.conf_thresh=0.015 #0.015
+        self.nms_dist=1
+        self.conf_thresh=0.005 #0.015
         self.nn_thresh=0.7
         
         use_cuda = torch.cuda.is_available() and do_cuda
